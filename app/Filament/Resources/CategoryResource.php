0ca3db195c59497f37cd3a2d83b93ce05d32f37c
Thiago F. da Rosa <thyagogoth@gmail.com>
2024-08-30 21:52:39 -0300

    alterado forma de exibição dos itens de ações (editar.. excluir...)

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -98,8 +98,10 @@ class CategoryResource extends Resource
                 ->slideOver()
             )
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -81,8 +81,10 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
